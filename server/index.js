3238e5b95dc075a497112ae6939a098f4ed59eb2
hraday04 <hraday04@gmail.com>
2025-08-01 17:50:33 +0530

    Terminal modified

--- server/index.js
+++ server/index.js
@@ -1,111 +1,91 @@
-const http = require('http')
-const express = require('express')
-const fs = require('fs/promises')
-const { Server } = require('socket.io')
-const path = require('path')
-const cors = require('cors')
-const chokidar = require('chokidar');
+const http = require("http");
+const express = require("express");
+const fs = require("fs/promises");
+const { Server: SocketServer } = require("socket.io");
+const path = require("path");
+const cors = require("cors");
+const chokidar = require("chokidar");
 
-const pty = require('node-pty')
-const port = process.env.PORT || 4000; 
+const pty = require("node-pty");
 
-const cwdPath = path.join(process.env.INIT_CWD || process.cwd(), 'user');
-
-fs.mkdir(cwdPath, { recursive: true });
+const userDir = path.resolve(__dirname, "user");
 
 
-const ptyProcess = pty.spawn('bash', [], {
-    name: 'xterm-color',
-    cols: 80,
-    rows: 30,
-    cwd: cwdPath,
-    env: process.env
+const ptyProcess = pty.spawn("bash", [], {
+  name: "xterm-color",
+  cols: 80,
+  rows: 30,
+  cwd: userDir,
+  env: process.env,
 });
 
-const app = express()
+
+
+const app = express();
 const server = http.createServer(app);
-// const io = new SocketServer({
-//     cors: '*'
-// })
-
-const io = new Server(server, {
-  cors: {
-    origin: "https://web-ide-amber.vercel.app/", 
-    methods: ["GET", "POST"],
-  },
+const io = new SocketServer({
+  cors: "*",
 });
 
-app.use(cors(
-    
-))
-// app.use(express.static("public")); 
-
-// app.get("/", (req, res) => {
-//   res.send("Welcome to Web‑IDE backend!");
-// });
+app.use(cors());
 
 io.attach(server);
 
-chokidar.watch('./user').on('all', (event, path) => {
-    io.emit('file:refresh', path)
+chokidar.watch("./user").on("all", (event, path) => {
+  io.emit("file:refresh", path);
 });
 
-ptyProcess.onData(data => {
-    io.emit('terminal:data', data)
-})
+ptyProcess.onData((data) => {
+  io.emit("terminal:data", data);
+});
 
-io.on('connection', (socket) => {
-    console.log(`Socket connected`, socket.id)
+io.on("connection", (socket) => {
+  console.log(`Socket connected`, socket.id);
 
-    socket.emit('file:refresh')
+  socket.emit("file:refresh");
 
-    socket.on('file:change', async ({ path, content }) => {
-        await fs.writeFile(`./user${path}`, content)
-    })
+  socket.on("file:change", async ({ path, content }) => {
+    await fs.writeFile(`./user${path}`, content);
+  });
 
-    socket.on('terminal:write', (data) => {
-        // console.log('Recieved data : ', data)
-        ptyProcess.write(data);
-    })
+  socket.on("terminal:write", (data) => {
+    // console.log("Term", data);
+    ptyProcess.write(data);
+  });
+});
 
-})
+app.get("/files", async (req, res) => {
+  const fileTree = await generateFileTree("./user");
+  return res.json({ tree: fileTree });
+});
 
-app.get('/files', async (req, res) => {
-    const fileTree = await generateFileTree('./user');
-    return res.json({ tree: fileTree })
-})
+app.get("/files/content", async (req, res) => {
+  const path = req.query.path;
+  const content = await fs.readFile(`./user${path}`, "utf-8");
+  return res.json({ content });
+});
 
-app.get('/files/content', async (req, res) => {
-    const path = req.query.path;
-    const content = await fs.readFile(`./user${path}`, 'utf-8')
-    return res.json({ content })
-})
-
-server.listen(9000, () => console.log(`server running on port 9000`))
-
+server.listen(9000, () => console.log(`Server running on port 9000`));
 
 async function generateFileTree(directory) {
-    const tree = {}
+  const tree = {};
 
-    async function buildTree(currentDir, currentTree) {
-        const files = await fs.readdir(currentDir)
+  async function buildTree(currentDir, currentTree) {
+    const files = await fs.readdir(currentDir);
 
-        for (const file of files) {
-            const filePath = path.join(currentDir, file)
-            const stat = await fs.stat(filePath)
+    for (const file of files) {
+      const filePath = path.join(currentDir, file);
+      const stat = await fs.stat(filePath);
 
-            if (stat.isDirectory()) {
-                currentTree[file] = {}
-                await buildTree(filePath, currentTree[file])
-            } else {
-                currentTree[file] = null
-            }
-        }
+      if (stat.isDirectory()) {
+        currentTree[file] = {};
+        await buildTree(filePath, currentTree[file]);
+      } else {
+        currentTree[file] = null;
+      }
     }
+  }
 
-    await buildTree(directory, tree);
-    return tree
+  await buildTree(directory, tree);
+  return tree;
 }
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
